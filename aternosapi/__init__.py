7c5480ed80fb87bd8c610030df6a37dfce2ab9b2
builderpepc <49398322+builderpepc@users.noreply.github.com>
2020-09-19 20:13:05 -0400

    Update __init__.py
    Add function descriptions and GetPlayers().

--- aternosapi/__init__.py
+++ aternosapi/__init__.py
@@ -14,6 +14,7 @@ class AternosAPI():
         self.CheckVaildInput()
 
     def CheckVaildInput(self):
+        """Determine if cookies are valid."""
         webserver = requests.get(url='https://aternos.org/server/',cookies=self.cookies,headers=self.headers)
         if ("logout" in webserver):
             pass
@@ -21,13 +22,29 @@ class AternosAPI():
             return "Invaild cookie"
 
     def GetStatus(self):
+        """Get server status as a str."""
         webserver = requests.get(url='https://aternos.org/server/',cookies=self.cookies,headers=self.headers)
         webdata = BeautifulSoup(webserver.content, 'html.parser')
         status = webdata.find('span', class_='statuslabel-label').get_text()
         status = status.strip()
         return status
     
+    def GetPlayers(self):
+        """Get list of player names."""
+        webserver = requests.get(url='https://aternos.org/players/',cookies=self.cookies,headers=self.headers)
+        webdata = BeautifulSoup(webserver.content, 'html.parser')
+        result = []
+        players = webdata.find('div', class_='playerlist')
+        players = players.findChildren("div", class_="player")
+        for p in players:
+            player = p.findChildren("div", class_="playerinfo")[0]
+            player = player.findChildren("div", class_="playername")[0].get_text()
+            player = player.strip()
+            result.append(player)
+        return result
+
     def StartServer(self):
+        """Skip queue and start server."""
         serverstatus = self.GetStatus()
         if serverstatus == "Online":
             return "Server Already Running"
@@ -36,6 +53,7 @@ class AternosAPI():
             self.skip_queue()
     
     def StopServer(self):
+        """Stop server."""
         serverstatus = self.GetStatus()
         if serverstatus == "Offline":
             return "Server Already Offline"
@@ -44,6 +62,7 @@ class AternosAPI():
             return "Server Stopped"
 
     def GetServerInfo(self):
+        """Get server IP, port, and software."""
         ServerInfo = requests.get(url='https://aternos.org/server/',cookies=self.cookies,headers=self.headers)
         ServerInfo = BeautifulSoup(ServerInfo.content, 'html.parser')
 
@@ -72,16 +91,19 @@ class AternosAPI():
             return f"{IP},{Port},{Software}"
 
     def queue_confirm(self):
+        """Confirm Aternos server start."""
         confirm = requests.get(url=f'https://aternos.org/panel/ajax/confirm.php?SEC={self.SEC}',cookies=self.cookies,headers=self.headers)
         return confirm.status_code
 
     def queue_number(self):
+        """Position in start queue."""
         webserver = requests.get(url='https://aternos.org/server/',cookies=self.cookies,headers=self.headers)
         webdata = BeautifulSoup(webserver.content, 'html.parser')
         status = webdata.find('span', class_='server-status-label-right').get_text()
         return status.strip()
 
     def skip_queue(self):
+        """Skip start queue."""
         i = 0
         while i < 1:
             serverstatus = self.GetStatus()
